e64163bf36b915fb28889fe502d29da54d2deda0
Arinta <arinta@atarraya.ai>
2019-12-13 22:01:17 -0600

    carpeta componentes

---
+++ src/components/registro.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Registro extends Component {
+    render(){
+       return ( 
+           <div>
+               <h2>Aqui va el registro</h2>
+           </div>
+       );
+    }
+}
+export default Registro;
