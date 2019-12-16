9982e161225aa1da8ed7712c431e068c969658eb
Arinta <arinta@atarraya.ai>
2019-12-16 15:48:05 -0600

    Pantalla  registro

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
 import Registro from './components/registro';
 
